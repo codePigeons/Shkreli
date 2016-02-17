50d55883be368b1d0759ae33402763e715e7cec9
Yan Philippe <yan.philippe@me.com>
2016-02-17 21:51:28 +0000

    More videos
    WEBM, OGV videos support

--- dist/src/shkreli.js
+++ dist/src/shkreli.js
@@ -1,5 +1,4 @@
 function Shkreli(objects) {
-    this.nodeType;
     this.element;
     this.video;
     this.width;
@@ -11,31 +10,26 @@ function Shkreli(objects) {
 
 Shkreli.prototype = {
     constructor: Shkreli,
-    enlarge: function () {
+    enlarge: function() {
         if (this.element) {
             if (this.element.charAt(0) == '#') {
                 // If div ID
                 this.width = document.getElementById(this.element.slice(1)).offsetWidth;
                 this.height = document.getElementById(this.element.slice(1)).offsetHeight;
-                this.nodeType = 'id';
             }
             return true;
         } else {
             return false;
         }
     },
-    broadcast: function (objects) {
+    broadcast: function(objects) {
         if (objects) {
-            if (objects.hasOwnProperty('video')) {
-                this.video = objects.video;
-            } else {
-                return false;
-            }
-            if (objects.hasOwnProperty('start') && objects.hasOwnProperty('end')) {
-                this.start = objects.start;
-                this.end = objects.end;
-            } else if (objects.hasOwnProperty('start')) {
-                this.start = objects.start;
+            this.video = objects;
+            if (this.video.hasOwnProperty('start') && this.video.hasOwnProperty('end')) {
+                this.start = this.video.start;
+                this.end = this.video.end;
+            } else if (this.video.hasOwnProperty('start')) {
+                this.start = this.video.start;
             }
             if (objects.hasOwnProperty('element')) {
                 this.element = objects.element;
@@ -59,16 +53,46 @@ Shkreli.prototype = {
                     shkreli.style.minHeight = "100%"
                     shkreli.style.height = this.height;
                     shkreli.style.webkitTransform = "translateX(-50%) translateY(-50%)";
-                    var mp4 = document.createElement('SOURCE');
-                    if (this.start && this.end) {
-                        mp4.src = this.video + '#t=' + this.start + ',' + this.end;
-                    } else if (this.start) {
-                        mp4.src = this.video + '#t=' + this.start;
-                    } else {
-                        mp4.src = this.video;
+                    if (this.video.hasOwnProperty('mp4')) {
+                        var mp4 = document.createElement('SOURCE');
+                        if (this.start && this.end) {
+                            mp4.src = this.video.mp4 + '#t=' + this.start + ',' + this.end;
+                        } else if (this.start) {
+                            mp4.src = this.video.mp4 + '#t=' + this.start;
+                        } else {
+                            mp4.src = this.video.mp4;
+                        }
+                        mp4.type = 'video/mp4';
+                        shkreli.appendChild(mp4);
                     }
-                    mp4.type = 'video/mp4';
-                    shkreli.appendChild(mp4);
+                    if (this.video.hasOwnProperty('ogv')) {
+                        var ogv = document.createElement('SOURCE');
+                        if (this.start && this.end) {
+                            ogv.src = this.video.ogv + '#t=' + this.start + ',' + this.end;
+                        } else if (this.start) {
+                            ogv.src = this.video.ogv + '#t=' + this.start;
+                        } else {
+                            ogv.src = this.video.ogv;
+                        }
+                        ogv.type = 'video/ogg';
+                        shkreli.appendChild(ogv);
+                    }
+                    if (this.video.hasOwnProperty('webm')) {
+                        var webm = document.createElement('SOURCE');
+                        if (this.start && this.end) {
+                            webm.src = this.video.webm + '#t=' + this.start + ',' + this.end;
+                        } else if (this.start) {
+                            webm.src = this.video.webm + '#t=' + this.start;
+                        } else {
+                            webm.src = this.video.webm;
+                        }
+                        webm.type = 'video/webm';
+                        shkreli.appendChild(webm);
+                    }
+                    if (this.video.hasOwnProperty('cover')) {
+                        alert('cover');
+                        newNode.setAttribute(this.video.cover);
+                    }
                 } else {
                     // Put Shkreli
                 }
